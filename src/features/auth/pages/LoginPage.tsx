56f2b836031e8d6875440cbdbe9b63c38ece1f4d
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-10-21 12:05:05 +0700

    Add MUI

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,9 +1,40 @@
+import { Button, Paper, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
 import React from 'react';
 
-export interface Props {}
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+  },
 
-const LoginPage = (props: Props) => {
-  return <div>LoginPage</div>;
+  box: {
+    padding: '24px',
+  },
+});
+
+const LoginPage = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper elevation={1} className={classes.box}>
+        <Typography variant="h5" component="h1">
+          Student Management
+        </Typography>
+
+        <Box mt={4}>
+          <Button fullWidth variant="contained" color="primary">
+            Fake Login
+          </Button>
+        </Box>
+      </Paper>
+    </div>
+  );
 };
 
 export default LoginPage;
