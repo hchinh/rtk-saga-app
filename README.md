900def71f13b32bf2c331fbc5b09471e1b32a0aa
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-10-26 21:52:21 +0700

    routing students

--- README.md
+++ README.md
@@ -30,3 +30,30 @@ LOGOUT
 
 - clear token from local storage
 - Redirect to login page
+
+## Students
+
+ROUTINGS
+
+- /admin/students: listing
+- /admin/students/add: add new student
+- /admin/students/:studentId: update a student
+
+LISTING
+
+- Search by name
+- Filter by city
+- Sort by name, mark
+- Pagination
+
+student slice state:
+
+- loading
+- list
+- pagination
+- filter {page: 1, limit: 10, ...}
+
+ADD/EDIT
+
+- React Hook Form v7
+- Yup
--- src/features/students/index.tsx
+++ src/features/students/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import AddEditPage from './pages/AddEditPage';
+import ListPage from './pages/ListPage';
 
 const StudentFeature = () => {
-  return <div>Student Feature</div>;
+  const match = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route path={match.path} exact>
+        <ListPage />
+      </Route>
+
+      <Route path={`${match.path}/add`}>
+        <AddEditPage />
+      </Route>
+
+      <Route path={`${match.path}/:studentId`}>
+        <AddEditPage />
+      </Route>
+    </Switch>
+  );
 };
 
 export default StudentFeature;
