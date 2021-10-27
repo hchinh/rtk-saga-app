15016e2b39e806b776bec4078349e5f0b305867f
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-10-27 09:51:20 +0700

    Student List UI

--- src/features/students/pages/ListPage.tsx
+++ src/features/students/pages/ListPage.tsx
@@ -1,9 +1,28 @@
-import { useAppDispatch } from 'app/hooks';
+import { Button, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/system';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 import React, { useEffect } from 'react';
-import { studentActions } from '../studentSlice';
+import StudentTable from '../components/StudentTable';
+import { selectStudentList, studentActions } from '../studentSlice';
+
+const useStyles = makeStyles(() => ({
+  root: {},
+
+  titleContainer: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    marginBottom: '32px',
+  },
+}));
 
 const ListPage = () => {
+  const studentList = useAppSelector(selectStudentList);
   const dispatch = useAppDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(
@@ -12,9 +31,23 @@ const ListPage = () => {
         _limit: 15,
       })
     );
-  });
+  }, [dispatch]);
 
-  return <div>List Page</div>;
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.titleContainer}>
+        <Typography variant="h4">Students</Typography>
+        <Button variant="contained" color="primary">
+          Add New Student
+        </Button>
+      </Box>
+
+      {/* Student table */}
+      <StudentTable studentList={studentList} />
+
+      {/* Student pagination */}
+    </Box>
+  );
 };
 
 export default ListPage;
