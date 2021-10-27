d19fee2cff07730bb8c7e18fd60f49d8d3a9e7e9
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-10-27 09:12:50 +0700

    Setup student saga

--- src/features/students/pages/ListPage.tsx
+++ src/features/students/pages/ListPage.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import { useAppDispatch } from 'app/hooks';
+import React, { useEffect } from 'react';
+import { studentActions } from '../studentSlice';
 
 const ListPage = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(
+      studentActions.fetchStudentList({
+        _page: 1,
+        _limit: 15,
+      })
+    );
+  });
+
   return <div>List Page</div>;
 };
 
