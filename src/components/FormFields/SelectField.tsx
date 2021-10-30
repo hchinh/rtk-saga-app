bd05e45eaa59c91fea2b07f4f66193ad27899249
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-10-30 17:07:18 +0700

    selectfield + validation

---
+++ src/components/FormFields/SelectField.tsx
@@ -0,0 +1,57 @@
+import { FormHelperText, MenuItem } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import React from 'react';
+import { Control, useController } from 'react-hook-form';
+
+export interface SelectOption {
+  label?: string;
+  value: number | string;
+}
+
+export interface SelectFieldProps {
+  name: string;
+  control: Control<any>;
+  label?: string;
+  disabled?: boolean;
+  options: SelectOption[];
+}
+
+export const SelectField = ({ name, control, label, disabled, options }: SelectFieldProps) => {
+  const {
+    field: { value, onChange, onBlur },
+    fieldState: { invalid, error },
+  } = useController({
+    name,
+    control,
+  });
+
+  return (
+    <FormControl
+      variant="outlined"
+      size="small"
+      fullWidth
+      disabled={disabled}
+      margin="normal"
+      error={invalid}
+    >
+      <InputLabel id={`${name}_label`}>{label}</InputLabel>
+      <Select
+        labelId={`${name}_label`}
+        label={label}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      >
+        {options.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <FormHelperText>{error?.message}</FormHelperText>
+    </FormControl>
+  );
+};
--- src/features/students/components/StudentForm.tsx
+++ src/features/students/components/StudentForm.tsx
@@ -1,9 +1,13 @@
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
-import { InputField, RadioGroupField } from 'components/FormFields';
+import { useAppSelector } from 'app/hooks';
+import { InputField, RadioGroupField, SelectField } from 'components/FormFields';
+import { selectCityOptions } from 'features/city/citySlice';
 import { Student } from 'models';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 export interface StudentFormProps {
   initialValues?: Student;
@@ -11,8 +15,39 @@ export interface StudentFormProps {
 }
 
 const StudentForm = ({ initialValues, onSubmit }: StudentFormProps) => {
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .required('Please enter name.')
+      .test('two-words', 'Please enter at least two words', (value) => {
+        if (!value) return true;
+
+        const parts = value?.split(' ') || [];
+        return parts.filter((x) => !!x).length >= 2;
+      }),
+    age: yup
+      .number()
+      .positive('Please enter a positive number.')
+      .integer('Please enter an integer.')
+      .min(18, 'Min is 18')
+      .max(60, 'Max is 60')
+      .required('Please enter age.')
+      .typeError('Please enter a valid number.'),
+    mark: yup
+      .number()
+      .min(0, 'Min is 0')
+      .max(10, 'Max is 10')
+      .required('Please enter mark.')
+      .typeError('Please enter a valid number.'),
+    gender: yup.string().oneOf(['male', 'female'], 'Please select either male or female.'),
+    city: yup.string().required('Please select city.'),
+  });
+
+  const cityOptions = useAppSelector(selectCityOptions);
+
   const { control, handleSubmit } = useForm<Student>({
     defaultValues: initialValues,
+    resolver: yupResolver(schema),
   });
 
   const handleFormSubmit = (formValues: Student) => {
@@ -34,7 +69,8 @@ const StudentForm = ({ initialValues, onSubmit }: StudentFormProps) => {
         />
         <InputField name="age" label="Age" control={control} type="number" />
         <InputField name="mark" label="Mark" control={control} type="number" />
-        <InputField name="city" label="City" control={control} />
+
+        <SelectField name="city" label="City" control={control} options={cityOptions} />
 
         <Box mt={3}>
           <Button type="submit" variant="contained" color="primary">
